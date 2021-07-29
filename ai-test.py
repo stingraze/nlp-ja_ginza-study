dfed65a61cf948813bbcd4632a6389837b5a815e
Tsubasa Kato <tsubasa@superai.online>
2021-07-30 07:31:08 +0900

    Update ai-test.py

--- ai-test.py
+++ ai-test.py
@@ -1,4 +1,4 @@
-#When faced with error, I had to update pip and install python-devel via yum (CentOS 7)
+#When faced with several errors, upon running, I had to update pip and install python3-devel via yum (CentOS 7)
 #Most of code from sample at: https://www.megagon.ai/jp/blog/ginza-version-4-0/
 import spacy
 import ginza
